3507ba10c13005a87fec7d2f12488b8b1584264b
mateusVedoy <mateusvedoy@outlook.com>
2023-06-10 13:17:21 -0300

    feat: implements financial operation model

---
+++ src/main/java/finances/api/infraestructure/postgres/model/FinancialOperationModel.java
@@ -0,0 +1,60 @@
+package finances.api.infraestructure.postgres.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "financialoperation")
+public class FinancialOperationModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "fop_code")
+    private Long id;
+
+    @Column(name = "fop_type")
+    private Long type;
+
+    @Column(name = "fop_amount")
+    private double amount;
+
+    @Column(name = "fop_executedAt")
+    private LocalDateTime executedAt;
+
+    public FinancialOperationModel(String type, double amount, LocalDateTime executedAt){
+        this.type = setType(type);
+        this.amount = amount;
+        this.executedAt = executedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getExecutedAt() {
+        return executedAt;
+    }
+
+    private Long setType(String type) {
+        if(type.equals("Entrance"))
+            return 1L;
+        else if(type.equals("Exit"))
+            return 2L;
+        return null;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public void setExecutedAt(LocalDateTime executedAt) {
+        this.executedAt = executedAt;
+    }
+}
