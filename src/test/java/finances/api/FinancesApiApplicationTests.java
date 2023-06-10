762b7b4e5139c4ae9a8088c058fb27435a7bd1dc
mateusVedoy <mateusvedoy@outlook.com>
2023-06-10 14:20:02 -0300

    test: initial unimplemented

---
+++ src/test/java/finances/api/FinancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package finances.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
