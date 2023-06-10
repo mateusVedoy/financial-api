8aac0217ef4534b20b5b96c79449e683cbd4dcec
mateusVedoy <mateusvedoy@outlook.com>
2023-06-10 13:19:15 -0300

    feat: main class of app

---
+++ src/main/java/finances/api/FinancesApiApplication.java
@@ -0,0 +1,13 @@
+package finances.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesApiApplication.class, args);
+	}
+
+}
