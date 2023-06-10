24c6e22df87f28cd5f26cced0cf448016559eca6
mateusVedoy <mateusvedoy@outlook.com>
2023-06-10 13:06:52 -0300

    feat: implements message response struct

---
+++ src/main/java/finances/api/application/response/Message.java
@@ -0,0 +1,19 @@
+package finances.api.application.response;
+
+public class Message {
+    private String message;
+    private String stacktrace;
+
+    public Message(String message, String stacktrace) {
+        this.message = message;
+        this.stacktrace = stacktrace;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getStacktrace() {
+        return stacktrace;
+    }
+}
