014b4ebf58425815373a885ea5529325fa9794cb
mateusVedoy <mateusvedoy@outlook.com>
2023-06-10 14:14:55 -0300

    feat: initial database request data

---
+++ init.sql
@@ -0,0 +1,3 @@
+-- GENERATE VALUES FOR OPERATIONTYPE TABLE
+insert into operationtype(ope_code, ope_description)values(1, 'Entrance');
+insert into operationtype(ope_code, ope_description)values(2, 'Exit');
