91f5d322c595cd06e0394d5887f8d95275eb26d7
mateusVedoy <mateusvedoy@outlook.com>
2023-06-14 00:34:37 -0300

    refactor: converter port to generic

--- src/main/java/finances/api/domain/port/IConverter.java
+++ src/main/java/finances/api/domain/port/IConverter.java
@@ -2,7 +2,8 @@ package finances.api.domain.port;
 
 import finances.api.application.dto.FinancialOperationDTO;
 import finances.api.domain.entity.FinancialOperation;
+import finances.api.domain.exception.BusinessValidationException;
 
-public interface IConverter {
-    FinancialOperationDTO convert(FinancialOperation model);
+public interface IConverterDTO <T, E>{
+    T convert(E dto) throws BusinessValidationException;
 }
--- src/main/java/finances/api/domain/port/IConverterDTO.java
+++
@@ -1,9 +0,0 @@
-package finances.api.domain.port;
-
-import finances.api.application.dto.FinancialOperationDTO;
-import finances.api.domain.entity.FinancialOperation;
-import finances.api.domain.exception.BusinessValidationException;
-
-public interface IConverterDTO {
-    FinancialOperation convert(FinancialOperationDTO dto) throws BusinessValidationException;
-}
