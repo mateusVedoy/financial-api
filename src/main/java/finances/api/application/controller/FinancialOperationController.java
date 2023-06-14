4ab1c0b7fe6a8d707a7494d5b6c734108bdb4583
mateusVedoy <mateusvedoy@outlook.com>
2023-06-14 00:43:05 -0300

    feat: new controller path to list all use case

--- src/main/java/finances/api/application/controller/FinancialOperationController.java
+++ src/main/java/finances/api/application/controller/FinancialOperationController.java
@@ -3,13 +3,11 @@ package finances.api.application.controller;
 import finances.api.application.dto.FinancialOperationDTO;
 import finances.api.application.response.APIResponse;
 import finances.api.application.useCase.CreateFinancialOperation;
+import finances.api.application.useCase.FindAllFinancialOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/finances/operation")
@@ -17,10 +15,18 @@ public class FinancialOperationController {
 
     @Autowired
     CreateFinancialOperation createFinancialOperation;
+    @Autowired
+    FindAllFinancialOperation findAllFinancialOperation;
 
     @PostMapping(value = "/save")
     public ResponseEntity save(@RequestBody FinancialOperationDTO dto) {
         APIResponse response = createFinancialOperation.save(dto);
+        return new ResponseEntity<>(response, HttpStatus.resolve(response.getStatus()));
+    }
+
+    @GetMapping(value = "/find/all")
+    public ResponseEntity findAll() {
+        APIResponse response = findAllFinancialOperation.findAll();
         return new ResponseEntity<>(response, HttpStatus.resolve(response.getStatus()));
     }
 }
