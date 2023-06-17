e1007aca1f794ee712010a3fe389c8c374d7e06f
mateusVedoy <mateusvedoy@outlook.com>
2023-06-17 12:12:01 -0300

    refactor: return messages and error handler

--- src/main/java/finances/api/application/useCase/FindAllFinancialOperation.java
+++ src/main/java/finances/api/application/useCase/FindAllFinancialOperation.java
@@ -3,8 +3,10 @@ package finances.api.application.useCase;
 import finances.api.application.converter.FinancialOperationConverter;
 import finances.api.application.dto.FinancialOperationDTO;
 import finances.api.application.response.APIResponse;
+import finances.api.application.response.ResponseError;
 import finances.api.application.response.ResponseSuccess;
 import finances.api.domain.entity.FinancialOperation;
+import finances.api.domain.exception.BusinessException;
 import finances.api.domain.exception.BusinessValidationError;
 import finances.api.infraestructure.repository.FinancialOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,11 @@ import java.util.List;
 
 @Component
 public class FindAllFinancialOperation {
+
+    private static final String EMPTY_SUCCESS = "There's no financial operation to be recovered";
+    private static final String SUCCESS = "Financial operation fetched bellow";
+    private static final String ERROR = "Something went wrong. Consult errors.";
+
     @Autowired
     private FinancialOperationRepository repository;
 
@@ -22,15 +29,19 @@ public class FindAllFinancialOperation {
     private FinancialOperationConverter converter;
 
     public APIResponse findAll() {
-        List<FinancialOperation> list = repository.findAll();
-        if(!isThereAnyValueToBeSolved(list))
-            return new ResponseSuccess<>(200,"There's no financial operation to be recovered");
-        else
-            return buildAPIResponseWithData(convertList(list));
+        try{
+            List<FinancialOperation> list = repository.findAll();
+            if(!isThereAnyValueToBeSolved(list))
+                return new ResponseSuccess<>(200,EMPTY_SUCCESS);
+            else
+                return buildAPIResponseWithData(convertList(list));
+        }catch (Exception ex) {
+            return new ResponseError(400, ERROR, ex);
+        }
     }
 
     private APIResponse buildAPIResponseWithData(List<FinancialOperationDTO> list) {
-        return new ResponseSuccess<FinancialOperationDTO>(200, "Financial operation fetched bellow", list);
+        return new ResponseSuccess<FinancialOperationDTO>(200, SUCCESS, list);
     }
     private List<FinancialOperationDTO> convertList(List<FinancialOperation> list) {
         List<FinancialOperationDTO> operationList = new ArrayList<>();
@@ -38,7 +49,7 @@ public class FindAllFinancialOperation {
             try {
                 operationList.add(converter.convert(value));
             } catch (BusinessValidationError e) {
-                throw new RuntimeException(e); //ajustar depois o tipo de erro
+                throw new BusinessException("Error during conversion process", "convert.entity.to.dto");
             }
         });
         return operationList;
