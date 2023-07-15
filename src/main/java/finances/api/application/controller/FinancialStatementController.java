7a815a3bf4fc2ce152be2eac25f3ab5ac9b026a4
mateusVedoy <mateusvedoy@outlook.com>
2023-07-15 09:38:00 -0300

    feat: modify use case into statement controller

--- src/main/java/finances/api/application/controller/FinancialStatementController.java
+++ src/main/java/finances/api/application/controller/FinancialStatementController.java
@@ -3,6 +3,7 @@ package finances.api.application.controller;
 
 import finances.api.application.response.APIResponse;
 import finances.api.application.useCase.FindFinancialOperationByPeriod;
+import finances.api.application.useCase.ProduceFinancialStatementByPeriod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class FinancialStatementController {
 
     @Autowired
-    private FindFinancialOperationByPeriod findFinancialOperationByPeriod;
+    private ProduceFinancialStatementByPeriod financialStatementByPeriod;
     @GetMapping("/balance")
     public ResponseEntity getBalancePerPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
-        APIResponse response = findFinancialOperationByPeriod.findByPeriod(initialDate, finalDate);
+        APIResponse response = financialStatementByPeriod.produce(initialDate, finalDate);
         return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 }
