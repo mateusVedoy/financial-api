9a1e05211d2b75288269c12c564cadccab570392
mateusVedoy <mateusvedoy@outlook.com>
2023-07-19 10:26:33 -0300

    refact: fix query from jpa repository interface

--- src/main/java/finances/api/infraestructure/postgres/IFinancialOperationRepository.java
+++ src/main/java/finances/api/infraestructure/postgres/IFinancialOperationRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 
 
 @Repository
 public interface IFinancialOperationRepository extends JpaRepository<FinancialOperationModel, Long> {
     @Query(nativeQuery = true,
-            value = "select * from financialoperation f where f.fop_executed_at between ':initial_date' and ':final_date'")
-    public List<FinancialOperationModel> findCodeAndAmountFromFinancialOperationByPeriod(@Param("initial_date") String initialDate, @Param("final_date") String finalDate) ;
+            value = "select * from financialoperation f where f.fop_executed_at between " + ":initialDate" + " and " + ":finalDate")
+    public List<FinancialOperationModel> findFinancialOperationModelByPeriod(@Param("initialDate") LocalDate initialDate, @Param("finalDate") LocalDate finalDate) ;
 }
