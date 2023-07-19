892785a241efd846b108e910e632470573f6ba03
mateusVedoy <mateusvedoy@outlook.com>
2023-07-18 21:58:32 -0300

    refact: param types from findByPeriod repository signature

--- src/main/java/finances/api/domain/port/IRepository.java
+++ src/main/java/finances/api/domain/port/IRepository.java
@@ -1,10 +1,11 @@
 package finances.api.domain.port;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IRepository<T> {
     T save(T entity);
     List<T> findAll();
     T findById(Long id);
-    List<T> findByPeriod(String initialDate, String finalDate);
+    List<T> findByPeriod(LocalDate initialDate, LocalDate finalDate);
 }
