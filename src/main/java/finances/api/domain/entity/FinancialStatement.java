29e82f1906c02338ebca23c96683864222f18012
mateusVedoy <mateusvedoy@outlook.com>
2023-07-19 10:32:08 -0300

    feat: set business rules about financial statement

--- src/main/java/finances/api/domain/entity/FinancialStatement.java
+++ src/main/java/finances/api/domain/entity/FinancialStatement.java
@@ -1,27 +1,73 @@
 package finances.api.domain.entity;
 
+import finances.api.domain.exception.BusinessException;
+
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class FinancialStatement {
+public class FinancialStatement extends Entity {
+
+    private static final String DATE_REGEX = "^\\d{4}-\\d{2}-\\d{2}$";
     private String state;
     private double balance;
     private LocalDate startDate;
     private LocalDate finalDate;
     private List<FinancialOperation> operations;
 
-    public FinancialStatement(){}
+    public FinancialStatement(){
+        super();
+    }
 
     public FinancialStatement(
             double balance,
-            LocalDate startDate,
-            LocalDate finalDate
+            String startDate,
+            String finalDate
     ){
+        super();
         this.balance = balance;
-        this.startDate = startDate;
-        this.finalDate = finalDate;
+        this.startDate = parseStringDateToLocalDate(startDate);
+        this.finalDate = parseStringDateToLocalDate(finalDate);
         setType();
+        validate();
+    }
+
+    public FinancialStatement(String startDate, String finalDate){
+        super();
+        this.startDate = parseStringDateToLocalDate(startDate);
+        this.finalDate = parseStringDateToLocalDate(finalDate);
+        validate();
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+        setType();
+    }
+    @Override
+    public void validate() {
+        if(startDate == null || finalDate == null)
+            addError("Financial statement dates cannot be null", "financialStatement.date");
+
+        if(startDate != null && finalDate != null)
+            if(startDate.isAfter(finalDate))
+                addError("Final date cannot be less than initial date", "financialStatement.dates");
+    }
+    private boolean wasDatesToPeriodInformedCorrectly(String date) {
+        Pattern pattern = Pattern.compile(DATE_REGEX);
+        Matcher dateMatcher = pattern.matcher(date);
+        return dateMatcher.matches();
+    }
+
+    private LocalDate parseStringDateToLocalDate(String dt) {
+        if(wasDatesToPeriodInformedCorrectly(dt))
+            return LocalDate.parse(dt);
+        else
+            addError(
+                    "Cannot cast wrong dates format. Required pattern YYYY-MM-DD",
+                    "financialStatement.dates"
+            );
+        return null;
     }
 
     private void setType() {
