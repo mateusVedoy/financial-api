c0dcaac2264e0de1e7c183193acc673658d39fc9
mateusVedoy <mateusvedoy@outlook.com>
2023-07-29 13:23:24 -0300

    feat: add total input/output to converter

--- src/main/java/finances/api/application/converter/FinancialStatementConverter.java
+++ src/main/java/finances/api/application/converter/FinancialStatementConverter.java
@@ -1,6 +1,7 @@
 package finances.api.application.converter;
 
 import finances.api.application.dto.FinancialStatementDTO;
+import finances.api.application.dto.FinancialStatementTotalDTO;
 import finances.api.domain.entity.FinancialStatement;
 import finances.api.domain.exception.BusinessValidationError;
 import finances.api.domain.port.IConverter;
@@ -10,6 +11,14 @@ import org.springframework.stereotype.Component;
 public class FinancialStatementConverter implements IConverter<FinancialStatementDTO, FinancialStatement> {
     @Override
     public FinancialStatementDTO convert(FinancialStatement obj) throws BusinessValidationError {
-        return new FinancialStatementDTO(obj.getState(), obj.getBalance(), obj.getStartDate().toString(), obj.getFinalDate().toString());
+        return new FinancialStatementDTO(
+                obj.getState(),
+                obj.getBalance(),
+                obj.getStartDate().toString(),
+                obj.getFinalDate().toString(),
+                new FinancialStatementTotalDTO(
+                        obj.getTotalInputAmount(),
+                        obj.getTotalOutputAmount()
+                ));
     }
 }
