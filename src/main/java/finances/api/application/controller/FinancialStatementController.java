663c1aeff89b6b6f2172014567e28d10cad01308
mateusVedoy <mateusvedoy@outlook.com>
2023-07-29 21:06:16 -0300

    feat: add cross origin to financial statement controller

--- src/main/java/finances/api/application/controller/FinancialStatementController.java
+++ src/main/java/finances/api/application/controller/FinancialStatementController.java
@@ -7,12 +7,10 @@ import finances.api.application.useCase.ProduceFinancialStatementByPeriod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/finances/statement")
 public class FinancialStatementController {
 
