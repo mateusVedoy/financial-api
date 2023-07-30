0a3138926975a0f320086405eca569ee45b37f0d
mateusVedoy <mateusvedoy@outlook.com>
2023-07-30 00:17:04 -0300

    feat: remove cache and implement converter from find financial operation by period

--- src/main/java/finances/api/application/useCase/FindFinancialOperationByPeriod.java
+++ src/main/java/finances/api/application/useCase/FindFinancialOperationByPeriod.java
@@ -1,24 +1,30 @@
 package finances.api.application.useCase;
 
+import finances.api.application.converter.FinancialOperationConverter;
+import finances.api.application.dto.FinancialOperationDTO;
 import finances.api.application.response.APIResponse;
 import finances.api.application.response.ResponseError;
 import finances.api.application.response.ResponseSuccess;
 import finances.api.application.response.StatusMessage;
 import finances.api.domain.entity.FinancialOperation;
+import finances.api.domain.exception.BusinessException;
+import finances.api.domain.exception.BusinessValidationError;
 import finances.api.infraestructure.repository.FinancialOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Cacheable(value = "cache")
 public class FindFinancialOperationByPeriod {
 
     @Autowired
     private FinancialOperationRepository repository;
+    @Autowired
+    private FinancialOperationConverter converter;
 
     public APIResponse findByPeriod(LocalDate initialDate, LocalDate finalDate) {
 
@@ -28,14 +34,29 @@ public class FindFinancialOperationByPeriod {
                     finalDate
             );
             if(isOperationsPerPeriodEmpty(operations))
-                return new ResponseSuccess<FinancialOperation>(200, StatusMessage.EMPTY_SUCCESS.getValue());
+                return new ResponseSuccess<>(200, StatusMessage.EMPTY_SUCCESS.getValue());
             else
-                return new ResponseSuccess<FinancialOperation>(200, StatusMessage.SUCCESS.getValue(), operations);
+                return buildAPIResponseWithData(convertList(operations));
         }catch (Exception ex) {
             return new ResponseError(400, StatusMessage.ERROR.getValue(), ex);
         }
-
     }
+    private APIResponse buildAPIResponseWithData(List<FinancialOperationDTO> list) {
+        return new ResponseSuccess<FinancialOperationDTO>(200, StatusMessage.SUCCESS.getValue(), list);
+    }
+    private List<FinancialOperationDTO> convertList(List<FinancialOperation> list) {
+        List<FinancialOperationDTO> operationList = new ArrayList<>();
+        list.forEach(value -> {
+            try {
+                operationList.add(converter.convert(value));
+            } catch (BusinessValidationError e) {
+                throw new BusinessException("Error during conversion process", "convert.entity.to.dto");
+            }
+        });
+        return operationList;
+    }
+
+    //TODO: implementar validação das datas
 
     private boolean isOperationsPerPeriodEmpty(List<FinancialOperation> operations){
         return operations.size() == 0;
