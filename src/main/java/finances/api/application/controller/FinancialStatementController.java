22c1767d1184e2d6739964a7ce997d3fbe760baa
mateusVedoy <mateusvedoy@outlook.com>
2023-08-01 10:33:58 -0300

    feat: add caching to balance

--- src/main/java/finances/api/application/controller/FinancialStatementController.java
+++ src/main/java/finances/api/application/controller/FinancialStatementController.java
@@ -4,7 +4,9 @@ package finances.api.application.controller;
 import finances.api.application.response.APIResponse;
 import finances.api.application.useCase.FindFinancialOperationByPeriod;
 import finances.api.application.useCase.ProduceFinancialStatementByPeriod;
+import finances.api.infraestructure.http.HTTPResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +19,9 @@ public class FinancialStatementController {
     @Autowired
     private ProduceFinancialStatementByPeriod financialStatementByPeriod;
     @GetMapping("/balance")
-    public ResponseEntity getBalancePerPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
+    @Cacheable(value = "cache")
+    public HTTPResponseEntity getBalancePerPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
         APIResponse response = financialStatementByPeriod.produce(initialDate, finalDate);
-        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
+        return new HTTPResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 }
