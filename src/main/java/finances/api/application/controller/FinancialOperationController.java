addc70beef86c061e4f1d3726612d3d54e0cee42
mateusVedoy <mateusvedoy@outlook.com>
2023-08-04 22:16:28 -0300

    refact: remove spring cache and http response entity as controller response

--- src/main/java/finances/api/application/controller/FinancialOperationController.java
+++ src/main/java/finances/api/application/controller/FinancialOperationController.java
@@ -6,10 +6,7 @@ import finances.api.application.useCase.CreateFinancialOperation;
 import finances.api.application.useCase.FindAllFinancialOperation;
 import finances.api.application.useCase.FindFinancialOperationById;
 import finances.api.application.useCase.FindFinancialOperationByPeriod;
-import finances.api.infraestructure.http.HTTPResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,32 +28,28 @@ public class FinancialOperationController {
     FindFinancialOperationByPeriod findFinancialOperationByPeriod;
 
     @PostMapping(value = "/save")
-    @CacheEvict(value = "cache", allEntries = true)
-    public HTTPResponseEntity save(@RequestBody FinancialOperationDTO dto) {
+    public ResponseEntity save(@RequestBody FinancialOperationDTO dto) {
         APIResponse response = createFinancialOperation.save(dto);
-        return new HTTPResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
+        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 
     @GetMapping(value = "/find/all")
-    @Cacheable(value = "cache")
-    public HTTPResponseEntity findAll() {
+    public ResponseEntity findAll() {
         APIResponse response = findAllFinancialOperation.findAll();
-        return new HTTPResponseEntity<>(response, HttpStatus.resolve(response.getStatus()));
+        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 
     @GetMapping(value = "/find/id/{financialOperationId}")
-    @Cacheable(value = "cache")
-    public HTTPResponseEntity findById(@PathVariable Long financialOperationId) {
+    public ResponseEntity findById(@PathVariable Long financialOperationId) {
         APIResponse response = findFinancialOperationById.findById(financialOperationId);
-        return new HTTPResponseEntity<>(response, HttpStatus.resolve(response.getStatus()));
+        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 
     @GetMapping(value = "/find/period")
-    @Cacheable(value = "cache")
-    public HTTPResponseEntity findByPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
+    public ResponseEntity findByPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
         LocalDate initial_date = LocalDate.parse(initialDate);
         LocalDate final_date = LocalDate.parse(finalDate);
         APIResponse response = findFinancialOperationByPeriod.findByPeriod(initial_date, final_date);
-        return new HTTPResponseEntity<>(response, HttpStatus.resolve(response.getStatus()));
+        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 }
