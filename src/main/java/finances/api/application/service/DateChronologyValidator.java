67de57b52e73d5561aaa8a8bd06830cc15fd2a96
mateusVedoy <mateusvedoy@outlook.com>
2023-08-10 22:09:32 -0300

    feat: date chronology validator impl

---
+++ src/main/java/finances/api/application/service/DateChronologyValidator.java
@@ -0,0 +1,30 @@
+package finances.api.application.service;
+
+import finances.api.domain.port.IValidator;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class DateChronologyValidator implements IValidator<LocalDate> {
+
+    @Override
+    public boolean isValid(List<LocalDate> objects) {
+        if(isListEmpty(objects))
+            return false;
+
+        for(int x = 0; x < objects.size(); x++){
+            if(!isDatesChronologyValid(objects.get(x), objects.get(x++)))
+                return false;
+        }
+
+        return true;
+    }
+    private boolean isListEmpty(List<LocalDate> list) {
+        return list.isEmpty();
+    }
+    private boolean isDatesChronologyValid(LocalDate initialDate, LocalDate finalDate) {
+        return initialDate.isBefore(finalDate);
+    }
+}
