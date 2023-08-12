de588c7c795d926100de1b849fc224c1b9a2cc46
mateusVedoy <mateusvedoy@outlook.com>
2023-08-12 17:20:13 -0300

    refact: remove cache from controller

--- src/main/java/finances/api/application/controller/FinancialStatementController.java
+++ src/main/java/finances/api/application/controller/FinancialStatementController.java
@@ -2,11 +2,8 @@ package finances.api.application.controller;
 
 
 import finances.api.application.response.APIResponse;
-import finances.api.application.useCase.FindFinancialOperationByPeriod;
 import finances.api.application.useCase.ProduceFinancialStatementByPeriod;
-import finances.api.infraestructure.http.HTTPResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +16,8 @@ public class FinancialStatementController {
     @Autowired
     private ProduceFinancialStatementByPeriod financialStatementByPeriod;
     @GetMapping("/balance")
-    @Cacheable(value = "cache")
-    public HTTPResponseEntity getBalancePerPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
+    public ResponseEntity<APIResponse> getBalancePerPeriod(@RequestParam("initialDate") String initialDate, @RequestParam("finalDate") String finalDate) {
         APIResponse response = financialStatementByPeriod.produce(initialDate, finalDate);
-        return new HTTPResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
+        return new ResponseEntity<APIResponse>(response, HttpStatus.resolve(response.getStatus()));
     }
 }
