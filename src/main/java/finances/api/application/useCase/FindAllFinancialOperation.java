77165e7ac58c66300b1331be205622d05113383b
mateusVedoy <mateusvedoy@outlook.com>
2023-08-17 22:49:54 -0300

    feat: extends abstract use case to find all use case

--- src/main/java/finances/api/application/useCase/FindAllFinancialOperation.java
+++ src/main/java/finances/api/application/useCase/FindAllFinancialOperation.java
@@ -6,51 +6,60 @@ import finances.api.application.response.APIResponse;
 import finances.api.application.response.ResponseError;
 import finances.api.application.response.ResponseSuccess;
 import finances.api.application.response.StatusMessage;
+import finances.api.application.service.CachingService;
+import finances.api.application.service.SerializationService;
 import finances.api.domain.entity.FinancialOperation;
 import finances.api.domain.exception.BusinessException;
 import finances.api.domain.exception.BusinessValidationError;
 import finances.api.infraestructure.repository.FinancialOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class FindAllFinancialOperation {
-    @Autowired
-    private FinancialOperationRepository repository;
-    @Autowired
-    private FinancialOperationConverter converter;
+public class FindAllFinancialOperation extends FinancialOperationAbstract {
+    private static final String CACHE_KEY= "FIND_ALL";
+
+    public FindAllFinancialOperation() {}
 
     public APIResponse findAll() {
+
         try{
+            if(hasCache(CACHE_KEY)){
+                byte[] bytes = getCache(CACHE_KEY);
+                List<FinancialOperationDTO> dto = deserialize(bytes);
+                return new ResponseSuccess<FinancialOperationDTO>(
+                        200,
+                        StatusMessage.SUCCESS.getValue(),
+                        dto
+                );
+            }
+
             List<FinancialOperation> list = repository.findAll();
-            if(!isThereAnyValueToBeSolved(list))
-                return new ResponseSuccess<>(200, StatusMessage.EMPTY_SUCCESS.getValue());
-            else
-                return buildAPIResponseWithData(convertList(list));
+            return buildResponse(list);
         }catch (Exception ex) {
             return new ResponseError(400, StatusMessage.ERROR.getValue(), ex);
         }
     }
 
-    private APIResponse buildAPIResponseWithData(List<FinancialOperationDTO> list) {
-        return new ResponseSuccess<FinancialOperationDTO>(200, StatusMessage.SUCCESS.getValue(), list);
+    private APIResponse buildResponse(List<FinancialOperation> list) {
+        if(!isThereAnyValueToBeSolved(list)){
+            return new ResponseSuccess<>(200, StatusMessage.EMPTY_SUCCESS.getValue());
+        } else {
+            List<FinancialOperationDTO> financialOperationDTOList = convertList(list);
+            setCache(CACHE_KEY, financialOperationDTOList);
+            return new ResponseSuccess<FinancialOperationDTO>(
+                    200,
+                    StatusMessage.SUCCESS.getValue(),
+                    financialOperationDTOList
+            );
+        }
     }
-    private List<FinancialOperationDTO> convertList(List<FinancialOperation> list) {
-        List<FinancialOperationDTO> operationList = new ArrayList<>();
-        list.forEach(value -> {
-            try {
-                operationList.add(converter.convert(value));
-            } catch (BusinessValidationError e) {
-                throw new BusinessException("Error during conversion process", "convert.entity.to.dto");
-            }
-        });
-        return operationList;
-    }
+
+
     private boolean isThereAnyValueToBeSolved(List<FinancialOperation> list) {
-        return list.size() > 0;
+        return !list.isEmpty();
     }
 }
