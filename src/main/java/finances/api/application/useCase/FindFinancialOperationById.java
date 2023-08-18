84cbef015a2a04952c5b0d33189b1dcbdb5e2c63
mateusVedoy <mateusvedoy@outlook.com>
2023-08-18 17:39:53 -0300

    feat: extends base use case to find financial operation by id

--- src/main/java/finances/api/application/useCase/FindFinancialOperationById.java
+++ src/main/java/finances/api/application/useCase/FindFinancialOperationById.java
@@ -6,6 +6,8 @@ import finances.api.application.response.APIResponse;
 import finances.api.application.response.ResponseError;
 import finances.api.application.response.ResponseSuccess;
 import finances.api.application.response.StatusMessage;
+import finances.api.application.service.CachingService;
+import finances.api.application.service.SerializationService;
 import finances.api.infraestructure.repository.FinancialOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -15,23 +17,39 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Component
-public class FindFinancialOperationById {
+public class FindFinancialOperationById extends FinancialOperationAbstract{
+
+    private static final String CACHE_KEY= "FIND_BY_ID_";
     @Autowired
     private FinancialOperationRepository repository;
-
     @Autowired
     private FinancialOperationConverter converter;
+    @Autowired
+    private CachingService caching;
+    @Autowired
+    private SerializationService<FinancialOperationDTO> serialization;
 
     public APIResponse findById(Long id) {
+        String cacheById = setCacheKey(id);
         try{
+            if(hasCache(cacheById)) {
+                byte[] bytes = getCache(cacheById);
+                List<FinancialOperationDTO> dto = deserialize(bytes);
+                return new ResponseSuccess<>(200, StatusMessage.SUCCESS.getValue(), dto);
+            }
              FinancialOperationDTO dto = converter.convert(
                     repository.findById(id)
             );
+            setCache(cacheById, List.of(dto));
              return new ResponseSuccess<>(200, StatusMessage.SUCCESS.getValue(), List.of(dto));
         }catch (NoSuchElementException ex) {
             return new ResponseSuccess<>(200, StatusMessage.EMPTY_SUCCESS.getValue(), List.of());
         }catch (Exception ex) {
             return new ResponseError(400, StatusMessage.ERROR.getValue(), ex);
         }
+    }
+    private String setCacheKey(Long id) {
+        String idStr = String.valueOf(id);
+        return CACHE_KEY.concat(idStr);
     }
 }
