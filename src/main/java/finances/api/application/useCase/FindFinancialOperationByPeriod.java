f13ec8fef1454a1ed5cda7efa6fa9c8e43744854
mateusVedoy <mateusvedoy@outlook.com>
2023-08-20 21:47:05 -0300

    feat: extends base use case to find financial operation by period

--- src/main/java/finances/api/application/useCase/FindFinancialOperationByPeriod.java
+++ src/main/java/finances/api/application/useCase/FindFinancialOperationByPeriod.java
@@ -6,27 +6,35 @@ import finances.api.application.response.APIResponse;
 import finances.api.application.response.ResponseError;
 import finances.api.application.response.ResponseSuccess;
 import finances.api.application.response.StatusMessage;
+import finances.api.application.service.DateChronologyValidator;
+import finances.api.application.service.DatePatternValidator;
 import finances.api.domain.entity.FinancialOperation;
 import finances.api.domain.exception.BusinessException;
 import finances.api.domain.exception.BusinessValidationError;
 import finances.api.infraestructure.repository.FinancialOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+//TODO: implementar cache
 @Component
-public class FindFinancialOperationByPeriod {
+public class FindFinancialOperationByPeriod extends FinancialOperationAbstract {
 
     @Autowired
-    private FinancialOperationRepository repository;
+    private DatePatternValidator datePatternValidator;
     @Autowired
-    private FinancialOperationConverter converter;
+    private DateChronologyValidator dateChronologyValidator;
 
     public APIResponse findByPeriod(LocalDate initialDate, LocalDate finalDate) {
+
+        if(!isDatePatternValid(initialDate, finalDate))
+            return new ResponseError(400, StatusMessage.ERROR.getValue(), new BusinessException("Cannot cast wrong dates format. Required pattern YYYY-MM-DD", "initialDate.and.finalDate"));
+
+        if(!isDateChronologyValid(initialDate, finalDate))
+            return new ResponseError(400, StatusMessage.ERROR.getValue(), new BusinessException("Final date cannot be less than initial date", "initialDate.and.finalDate"));
 
         try{
             List<FinancialOperation> operations = repository.findByPeriod(
@@ -44,20 +52,31 @@ public class FindFinancialOperationByPeriod {
     private APIResponse buildAPIResponseWithData(List<FinancialOperationDTO> list) {
         return new ResponseSuccess<FinancialOperationDTO>(200, StatusMessage.SUCCESS.getValue(), list);
     }
-    private List<FinancialOperationDTO> convertList(List<FinancialOperation> list) {
-        List<FinancialOperationDTO> operationList = new ArrayList<>();
-        list.forEach(value -> {
-            try {
-                operationList.add(converter.convert(value));
-            } catch (BusinessValidationError e) {
-                throw new BusinessException("Error during conversion process", "convert.entity.to.dto");
-            }
-        });
-        return operationList;
+
+    private boolean isDatePatternValid(LocalDate initialDate, LocalDate finalDate) {
+        if(isDateEmpty(initialDate, finalDate))
+            return false;
+
+        return datePatternValidator.isValid(
+                List.of(
+                        initialDate.toString(),
+                        finalDate.toString())
+        );
+    }
+    private boolean isDateChronologyValid(LocalDate initialDate, LocalDate finalDate) {
+        if(isDateEmpty(initialDate, finalDate))
+            return false;
+
+        return dateChronologyValidator.isValid(
+                List.of(
+                        initialDate,
+                        finalDate)
+        );
     }
 
-    //TODO: implementar validação das datas
-
+    private boolean isDateEmpty(LocalDate initialDate, LocalDate finalDate) {
+        return initialDate == null || finalDate == null;
+    }
     private boolean isOperationsPerPeriodEmpty(List<FinancialOperation> operations){
         return operations.size() == 0;
     }
